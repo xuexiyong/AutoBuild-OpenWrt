be0ef1060213eae0747c368ac44c249939fb8db5
xuexiyong <19756594+xuexiyong@users.noreply.github.com>
2020-02-18 13:17:40 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,4 +7,4 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.7.1/g' openwrt/package/base-files/files/bin/config_generate
-sed -i 's/root::0:0:99999:7:::/root::0:0:99999:7:::/g' /etc/shadow
+# sed -i 's/root::0:0:99999:7:::/root::0:0:99999:7:::/g' /etc/shadow
