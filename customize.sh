3e0b18fe174c757550073e756f4dbb7076edfc4a
wakan.qi@gmail.com <19756594+xuexiyong@users.noreply.github.com>
2020-03-01 18:47:01 +0800

    实验

--- customize.sh
+++ customize.sh
@@ -7,8 +7,6 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.7.1/g' openwrt/package/base-files/files/bin/config_generate
-# sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root::0:0:99999:7:::/g' /etc/shadow
-# sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release
-# echo "DISTRIB_REVISION='R20.2.15'" >> /etc/openwrt_release
-# sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release
-# echo "DISTRIB_DESCRIPTION='OpenWrt '" >> /etc/openwrt_release
+sed -i 's/root::0:0:99999:7:::/root::0:0:99999:7:::/g' /etc/shadow
+git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+git clone https://github.com/rosywrt/luci-theme-rosy.git package/luci-theme-rosy
